c5b2214eff7b636bd0d9b7c7d7280fcfdb5a89e7
mllimo <ar.guijarro.contreras@gmail.com>
2021-12-21 23:31:16 +0000

    Guardado en base de datos funcional

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
-const bcrypt = require('bcryptjs');
 const { request, resonse } = require('express');
+const User = require('../models/user').Users;
+const bcrypt = require('bcryptjs');
+
 
 async function postLogin(req, res) {
   res.json({
@@ -8,9 +10,23 @@ async function postLogin(req, res) {
 }
 
 async function postSingUp(req, res) {
-  res.json({
-    message: 'Signup successful'
-  });
+  const body = req.body;
+  const user = new User(body);
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(body.password, salt);
+
+  try {
+    await user.save();
+    res.json({
+      message: 'User created'
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message
+    });
+  }
+
 }
 
 module.exports = {
