e251b122bc39a26e42f70c66d61eece602ad1780
mllimo <ar.guijarro.contreras@gmail.com>
2021-12-22 19:03:11 +0000

    controller user

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,19 @@
 const { request, resonse } = require('express');
 const checker = require('../helpers/checker');
 const User = require('../models/user').Users;
+const Content = require('../models/content').Contents;
 const utils = require('../helpers/utils');
 const bcrypt = require('bcryptjs');
 
 
+async function postUser( req = request, response = response) {
+  const body = req.body;
+  const content = new Content(body);
+  console.log(content);
+  // Conectar con API de series para tener un titulo y caratula adecuados
+}
+
 
 module.exports = {
-
+  postUser
 }
--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,14 @@
 const user_controller = require('../controllers/user');
-const db_validator = require('../validators/db');
 const fields_validator = require('../validators/fields');
 const { check } = require('express-validator');
 const { Router } = require('express');
 
 const router = new Router();
 
-router.post('/:username', (req, res) => {
-  const username = req.params.username;
-  res.json({ message: `User ${username}` });
-});
+router.post('/:username', [
+  fields_validator.validateJwt,
+  check('title').notEmpty().withMessage('Title is required'),
+], user_controller.postUser);
 
 
 
