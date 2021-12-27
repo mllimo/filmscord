4fadab4ed6bfc351b18b1f13d606fc0b18954a62
mllimo <ar.guijarro.contreras@gmail.com>
2021-12-27 09:17:07 +0000

    Arreglando fallos menores

--- test/user-controller.test.js
+++ test/user-controller.test.js
@@ -1,12 +1,10 @@
-require('dotenv').config({ path: `${__dirname}.env.test` });
-const chaiHttp = require('chai-http');
+require('dotenv').config({ path: `${__dirname}.env.test` })
+const chai_http = require('chai-http');
 const chai = require('chai');
-const mocha = require('mocha');
 
-
-const describe = mocha.describe;
+chai.use(chai_http); 
+const describe = require('mocha').describe;
 const expect = chai.expect;
-chai.use(chaiHttp);
 
 const BASE_URL = 'http://localhost:' + process.env.PORT + '/api/signup';
 const SIGNUP_URL = '/api/signup';
@@ -15,12 +13,13 @@ describe('UserController', () => {
   it('It should signup a user', (done) => {
     chai.request(BASE_URL)
       .post(SIGNUP_URL)
+      .type('json')
       .send({
         username: 'test',
         password: 'test',
         email: 'test@test.es'
       })
-      .end((err, res) => {
+      .then((res) => {
         expect(res).to.have.status(200);
         expect(res.body).to.have.property('authorization');
       });
