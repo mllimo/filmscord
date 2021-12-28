518b2530102bb5a0d7e7995201489119980e9181
mllimo <ar.guijarro.contreras@gmail.com>
2021-12-28 17:35:11 +0000

    Testeado POST /api/user

--- test/user.test.js
+++ test/user.test.js
@@ -16,12 +16,14 @@ describe('User', function () {
   const title_1 = 'Spiderman';
   const category_1 = 'movie';
   let content_1 = {};
+  let content_2 = {};
   let token;
 
   beforeEach(async () => {
     await User.deleteMany({});
     await Content.deleteMany({});
-    content_1 = { title: { text: title_1 }, category_1 };
+    content_1 = { title: { text: title_1 }, category: category_1 };
+    content_2 = { title: { text: title_1 }, category: category_1, rate: 5, comment: 'test comment' };
     const user = new User({ username, email, password });
     const content = new Content({ title: { text: title_1 }, category: category_1 });
     token = await utils.generateToken(user._id);
@@ -29,18 +31,37 @@ describe('User', function () {
     await content.save();
   });
 
-  describe('POST /api/user',  () => {
-    it ('It should add content', async function() {
+  describe('POST /api/user', () => {
+    it('It should add content with the title and categoty', async function () {
       chai.request(server)
-      .post(USER_URL)
-      .type('json')
-      .set('authorization', token)
-      .send(content_1)
-      .then((res) => {
-        expect(res).to.have.status(200);
-        done();
-      })
-      .catch((err) => err);
+        .post(USER_URL)
+        .type('json')
+        .set('authorization', token)
+        .send(content_1)
+        .then((res) => {
+          expect(res).to.have.status(200);
+          const added = User.findOne({ username }).contents
+          expect(added).to.have.lengthOf(1);
+          done();
+        })
+        .catch((err) => err);
+    });
+
+    it('It should add content with all the info', async function () {
+      chai.request(server)
+        .post(USER_URL)
+        .type('json')
+        .set('authorization', token)
+        .send(content_2)
+        .then((res) => {
+          expect(res).to.have.status(200);
+          const added = User.findOne({ username }).contents
+          expect(added).to.have.lengthOf(1);
+          expect(added[0].rate).to.equal(5);
+          expect(added[0].comment).to.equal('test comment');
+          done();
+        })
+        .catch((err) => err);
     });
   });
 
